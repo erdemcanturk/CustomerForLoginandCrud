f189ed7f5a2f6c64bf22acfcef46457c1962281a
erdem <canturkerdem@hotmail.com>
2022-01-10 05:47:28 +0300

    duzeltme

--- src/main/java/com/crud/koctasbackend/KoctasBackendApplication.java
+++ src/main/java/com/crud/koctasbackend/KoctasBackendApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class KoctasBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
